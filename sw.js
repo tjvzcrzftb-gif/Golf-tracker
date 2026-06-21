e9a235126657f05dec7ab5d3210830d518585d24
tjvzcrzftb-gif <tjvzcrzftb@privaterelay.appleid.com>
2026-06-21 01:26:09 -0600

    Update cache version and asset paths in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'golf-tracker-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'golf-tracker-v3';
+const ASSETS = ['./', './index.html', './manifest.json'];
  
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
